3662b1dd721b88991865b66a800428343f36b90b
Anto <antonella.cortes@udla.edu.ec>
2023-01-17 17:08:19 -0500

    Add Burger Services to connect with and manage the DB and its operations.

---
+++ AC_Hamburguesa/ACServices/ACBurgerServices.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using AC_Hamburguesa.ACModels;
+
+namespace AC_Hamburguesa.ACServices
+{
+    public class ACBurgerServices : ACIBurgerServices
+    {
+        private SQLiteAsyncConnection conn;
+
+        private async Task SetUpDb()
+        {
+            if (conn == null)
+            {
+                string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "burger.db3");
+                conn = new SQLiteAsyncConnection(dbPath);
+                await conn.CreateTableAsync<ACBurger>();
+            }
+        }
+
+        public async Task<int> AddBurger(ACBurger burgerModel)
+        {
+            await SetUpDb();
+            return await conn.InsertAsync(burgerModel);
+        }
+
+        public async Task<int> DeleteBurger(ACBurger burgerModel)
+        {
+            await SetUpDb();
+            return await conn.DeleteAsync(burgerModel);
+        }
+
+        public async Task<List<ACBurger>> GetACBurgersList()
+        {
+            await SetUpDb();
+            var burgerList = await conn.Table<ACBurger>().ToListAsync();
+            return burgerList;
+        }
+
+        public async Task<int> UpdateBurger(ACBurger burgerModel)
+        {
+            await SetUpDb();
+            return await conn.UpdateAsync(burgerModel);
+        }
+    }
+}
---
+++ AC_Hamburguesa/ACServices/ACIBurgerServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AC_Hamburguesa.ACModels;
+
+namespace AC_Hamburguesa.ACServices
+{
+    public interface ACIBurgerServices
+    {
+        Task<List<ACBurger>> GetACBurgersList();
+        Task<int> AddBurger(ACBurger burgerModel);
+        Task<int> DeleteBurger(ACBurger burgerModel);
+        Task<int> UpdateBurger(ACBurger burgerModel);
+    }
+}
