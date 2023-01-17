3f69e21795476b4f6dec2178a8e9a6525799ebde
Anto <antonella.cortes@udla.edu.ec>
2023-01-17 16:32:15 -0500

    Add SearchHandler to find Burgers and perform CRUD operations.

---
+++ AC_Hamburguesa/ACSearchHandlers/ACBurgerSearchHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AC_Hamburguesa.ACModels;
+
+namespace AC_Hamburguesa.ACSearchHandlers
+{
+    public class ACBurgerSearchHandler : SearchHandler
+    {
+        public IList<ACBurger> ACBurgers { get; set; }
+        public string NavigationRoute { get; set; }
+        public Type NavigationType { get; set; }
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            base.OnQueryChanged(oldValue, newValue);
+
+            if (string.IsNullOrWhiteSpace(newValue))
+            {
+                ItemsSource = null;
+            }
+            else
+            {
+                ItemsSource = ACBurgers.Where(burger => burger.Name.ToLower().Contains(newValue.ToLower())).ToList();
+            }
+        }
+
+        protected override async void OnItemSelected(object item)
+        {
+            base.OnItemSelected(item);
+            var navParam = new Dictionary<string, object>();
+            navParam.Add("ACBurgerDetail", item);
+            if (!string.IsNullOrWhiteSpace(NavigationRoute))
+            {
+                await Shell.Current.GoToAsync(NavigationRoute, navParam);
+            }
+        }
+    }
+}
